7f4343a1a19b9895663f661f2f291344eca29b58
ochapman2dv <115467704+ochapman2dv@users.noreply.github.com>
2025-10-06 16:26:26 +0100

    wasd

---
+++ script.js
@@ -0,0 +1,6 @@
+function loadPage2() {
+    document.getElementsByID("location").innerHTML = "You're Currently Visiting: Room 2";
+}
+function loadPage1() {
+    document.getElementsByID("location").innerHTML = "You're Currently Visiting: Room 1";
+}
