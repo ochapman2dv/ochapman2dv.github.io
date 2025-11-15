95cc43af07fdd747982e64e7b4ef805f71f72725
ochapman2dv <115467704+ochapman2dv@users.noreply.github.com>
2025-11-15 21:39:35 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function loadPage2() {
-    document.getElementsByID("location").innerHTML = "You're Currently Visiting: Room 2";
-}
 function loadPage1() {
-    document.getElementsByID("location").innerHTML = "You're Currently Visiting: Room 1";
+    document.title = "Room 1 - Oli's House";
+    document.querySelector("link[rel*='icon']").href = "./media/favicon.ico";
+    document.getElementById("room1").style.display = "block";
+    document.getElementById("room2").style.display = "none";
+    document.getElementById("location").innerHTML = "You're Currently Visiting: Room 1";
 }
+
+function loadPage2() {
+    document.title = "Room 2 - Oli's House";
+    document.querySelector("link[rel*='icon']").href = "./media/favicon2.ico";
+    document.getElementById("room1").style.display = "none";
+    document.getElementById("room2").style.display = "block";
+    document.getElementById("location").innerHTML = "You're Currently Visiting: Room 2";
+}
+
+//change loadPage function to take a variable parameters of what page to have displayed
+//add page sharing
