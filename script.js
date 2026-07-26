db6510034c6f110d4f9d102c1c4370abf9586260
ochapman2dv <oliverchapman2006@gmail.com>
2026-07-26 04:00:24 +0100

    cleaned up js more

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 let currentRoom = "room1" // set room1 to be default room
-let urlParameter = new URLSearchParams(window.location.search).get('page');
-// get URL parameter
+let urlParameter = new URLSearchParams(window.location.search).get("page"); // get initial URL parameter
 
-function goRoom(roomID,roomName) {
-    document.getElementById(roomID).style.display = "block";
-    document.getElementById(currentRoom).style.display = "none";
-    document.title = roomName+" - Oli's House";
-    document.getElementById("location").innerHTML = "You're Currently Visiting: "+roomName;
-    document.querySelector("link[rel*='icon']").href = "./media/"+roomID+".ico";
-    currentRoom = roomID;
-    // add current room to url parameter here
-    new URLSearchParams(window.location.search).set("room", roomID);
-    console.log()
+// map of all room IDs and their names
+const roomMap = new Map([
+    ["room1", "Room 1"],
+    ["room2", "Room 2"]
+
+]);
+
+function goRoom(roomID) {
+
+    roomName = getRoomName(roomID) // get room ID's associated name
+
+    document.getElementById(roomID).style.display = "block"; // show page content
+    document.getElementById(currentRoom).style.display = "none"; // hide other page content
+    document.title = roomName+" - Oli's House"; // change title of page
+    document.getElementById("location").innerHTML = "You're Currently Visiting: "+roomName; // change location text
+    document.querySelector("link[rel*='icon']").href = "./media/"+roomID+".ico"; // change favicon
+    currentRoom = roomID; // set currentRoom to roomID
+    
+
+    link = new URL(window.location.href), link.searchParams.set("page", currentRoom);
+    window.history.replaceState(null, "", link); // update url with new page link
 }
 
-function getRoomName(roomID) { // ugly ugly code that gets a room's name from its ID
-    if (roomID == 'room1') {
-        return "Room 1";
-    }
-    if (roomID == 'room2') {
-        return "Room 2";
-    }
-    else {
-        return null;
-    }
+
+function getRoomName(roomID) {
+
+if (roomMap.has(roomID)) {
+    return (roomMap.get(roomID));
+}
+else {
+    return null;
+}
 }
 
 if (getRoomName(urlParameter) == null) {
@@ -39,3 +48,7 @@ else {
     let currentRoom = urlParameter;
     goRoom(urlParameter,getRoomName(urlParameter));
 }
+
+
+// room isn't really a word to me anymore
+// hope you enjoyed my messy code xx
