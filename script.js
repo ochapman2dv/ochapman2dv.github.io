0d4402e63456bf6e9902db098b32090a328869d6
ochapman2dv <oliverchapman2006@gmail.com>
2026-07-26 03:30:08 +0100

    big overhaul of javascript and such

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-function loadPage1() {
-    document.title = "Room 1 - Oli's House";
-    document.querySelector("link[rel*='icon']").href = "./media/favicon.ico";
-    document.getElementById("room1").style.display = "block";
-    document.getElementById("room2").style.display = "none";
-    document.getElementById("location").innerHTML = "You're Currently Visiting: Room 1";
+let currentRoom = "room1" // set room1 to be default room
+let urlParameter = new URLSearchParams(window.location.search).get('page');
+// get URL parameter
+
+function goRoom(roomID,roomName) {
+    document.getElementById(roomID).style.display = "block";
+    document.getElementById(currentRoom).style.display = "none";
+    document.title = roomName+" - Oli's House";
+    document.getElementById("location").innerHTML = "You're Currently Visiting: "+roomName;
+    document.querySelector("link[rel*='icon']").href = "./media/"+roomID+".ico";
+    currentRoom = roomID;
+    // add current room to url parameter here
+    new URLSearchParams(window.location.search).set("room", roomID);
+    console.log()
 }
 
-function loadPage2() {
-    document.title = "Room 2 - Oli's House";
-    document.querySelector("link[rel*='icon']").href = "./media/favicon2.ico";
-    document.getElementById("room1").style.display = "none";
-    document.getElementById("room2").style.display = "block";
-    document.getElementById("location").innerHTML = "You're Currently Visiting: Room 2";
+function getRoomName(roomID) { // ugly ugly code that gets a room's name from its ID
+    if (roomID == 'room1') {
+        return "Room 1";
+    }
+    if (roomID == 'room2') {
+        return "Room 2";
+    }
+    else {
+        return null;
+    }
 }
 
-//change loadPage function to take a variable parameters of what page to have displayed
-//add page sharing
+if (getRoomName(urlParameter) == null) {
+    console.log("no parameter found!")
+    // oops, no parameter! (puts user to default room)
+}
+
+if (urlParameter == "room1") {
+    // do nothing, already at default room!
+}
+
+else {
+    let currentRoom = urlParameter;
+    goRoom(urlParameter,getRoomName(urlParameter));
+}
